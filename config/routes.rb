78a653de185a4a712b9ed7322ba179e440b5d19a
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-11-28 15:15:48 -0500

    decided on dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
+  root 'devise/sessions#new'
 
   resources :boats, except: [:edit, :update, :destroy]
+  resources :boats, except: [:edit, :update, :destroy]
+  get
 end
