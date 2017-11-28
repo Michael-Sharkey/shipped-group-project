a486085b61014f297075419f144248e0ebe7096d
natalietate <martin.nat@gmail.com>
2017-11-28 12:11:06 -0500

    create job table and update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128165023) do
+ActiveRecord::Schema.define(version: 20171128171016) do
 
   create_table "boats", force: :cascade do |t|
     t.string "name"
     t.integer "containers"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "origin"
+    t.string "destination"
+    t.integer "cost"
+    t.integer "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
