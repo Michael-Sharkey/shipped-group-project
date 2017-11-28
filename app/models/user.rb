663b672071e02f174250a1cb26b61be3abcecbf4
msalam <ms17alam@gmail.com>
2017-11-28 12:31:05 -0500

    User update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  has_and_belongs_to_many :boats
+  has_and_belongs_to_many :jobs
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
