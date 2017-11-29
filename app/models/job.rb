83e3e0138f585e12b782a609aa04ed3b8dc9b18b
msalam <ms17alam@gmail.com>
2017-11-29 13:07:15 -0500

    validates job

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,9 @@ class Job < ApplicationRecord
 
   belongs_to :user
   has_many :boats, through: :shipments
-  
+
+
+  validates :name, presence: true, uniqueness: true, uniqueness: { case_sensitive: false }, length: { minimum: 3}
+  validates :cost, presence: true,  numericality: { only_integer: true, greater_than: 1000 }
+  validates :description,   length: { minimum: 20}
 end
