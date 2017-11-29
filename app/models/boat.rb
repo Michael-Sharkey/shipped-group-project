15c456f0b937da3b24024a4ddb13e0e7cd9e6524
natalietate <martin.nat@gmail.com>
2017-11-29 12:44:10 -0500

    upload image with boat

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,7 +1,6 @@
 class Boat < ApplicationRecord
 
   belongs_to :user
-  belongs_to :shipment
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
